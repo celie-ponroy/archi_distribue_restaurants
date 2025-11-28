abe4919070d33cf6914e026efc58f14466eac351
Célie <celie.ponroy@laposte.net>
2025-11-28 16:17:38 +0100

    ajout avis

--- data/init.sh
+++ data/init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Importe les données depuis db.json
-mongoimport --db archiDistriRestaurants --collection user --file /docker-entrypoint-initdb.d/db.json --jsonArray
+mongoimport --db archiDistriRestaurants --collection user --file /docker-entrypoint-initdb.d/db_user.json --jsonArray
+mongoimport --db archiDistriRestaurants --collection avis --file /docker-entrypoint-initdb.d/db_avis.json --jsonArray
